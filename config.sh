db9369f3df584035d80018998adc2b02b1071e70
VARUN GAUR <101390136+doctorstrange2003@users.noreply.github.com>
2024-03-11 22:24:49 +0530

    Update config.sh
    Updated to 12.1

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://gitlab.com/OrangeFox/device/miatoll.git -b fox_12.1"
 
-export DEVICE="beryllium"
+export DEVICE="miatoll"
 export OEM="xiaomi"
 
 # Build Target
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=#TeamREDHive"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
